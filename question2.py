3ea57a78c73f5d1c3d0b90206cbd9b248266affb
dseymo02 <dseymo02@dcs.bbk.ac.uk>
2018-05-27 17:43:24 +0100

    passed test and created main()

--- question2.py
+++ question2.py
@@ -1,4 +1,9 @@
 '''code for question 2'''
 
-def check():
-	pass
+def check(number,guess):
+	if guess < number:
+		return "Too low"
+	elif guess > number:
+		return "Too high"
+	else:
+		return "Correct"
