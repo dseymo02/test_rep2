77b3385b5d0e5ee6f6525c45d63ad13af4933c25
dseymo02 <dseymo02@dcs.bbk.ac.uk>
2018-05-27 17:52:37 +0100

    finished with while loop

--- question2.py
+++ question2.py
@@ -15,6 +15,14 @@ def main():
 	guess = input("Guess a number between 0 and 99: ")
 	finished = False
 	while not finished:
-		
+		count += 1
+		if guess < number:
+			guess = input("Too low, Guess again: ")
+		elif guess > number:
+			guess = input("Too high, Guess again: ")
+		else:
+			finished = True
+	print("Correct. It took you, ",count," guesses")
+
 if __name__ == "__main__":
 	main()
