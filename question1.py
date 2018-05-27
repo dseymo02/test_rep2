5842ef949092421178a661b19270a9503cf1a4fa
dseymo02 <dseymo02@dcs.bbk.ac.uk>
2018-05-27 17:31:01 +0100

    saved test this time

---
+++ question1.py
@@ -0,0 +1,8 @@
+'''code for question 1'''
+
+def search(str,lst,size):
+	pass
+
+
+if __name__ == "__main__":
+	main()
--- test.py
+++ test.py
@@ -4,3 +4,8 @@ from question3 import *
 from question4 import *
 import pytest
 
+def search():
+	string = "hello"
+	lst = ["hi","holla","hello","bon day"]
+	size = len(lst)
+	assert search(string, lst, size) == 2
