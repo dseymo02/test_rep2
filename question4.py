d6a7058ddf5f14c8ec36be7dcc6b937a24b560fa
dseymo02 <dseymo02@dcs.bbk.ac.uk>
2018-05-27 18:02:32 +0100

    created main()

---
+++ question4.py
@@ -0,0 +1,19 @@
+'''code for question 4'''
+
+def readRecord():
+	pass
+
+def computeRegular():
+	pass
+
+def computePremium():
+	pass
+
+def printrecord():
+	pass
+
+def main():
+	pass
+
+if __name__ == "__main__":
+	main()
